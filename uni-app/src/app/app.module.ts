a84ff6e773f1075a495e17bee1f5ebe61bf279e7
Akshay Krishnan <akshaykrishnan@ceknpy.ac.in>
2022-09-17 23:26:05 +0530

    inital layout

--- uni-app/src/app/app.module.ts
+++ uni-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { HeroComponent } from './content/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
